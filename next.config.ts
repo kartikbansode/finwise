cdcc366d5b9fd815e5239444f379186703435c0a
kartikbansode <bansodekartik00@gmail.com>
2026-06-27 22:58:39 +0530

    UI/UX improvement & adding new components in settings page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ubxvqqqqpjxukjkmnqgx.supabase.co/",   // Your Supabase project
+      },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",   // This covers all Supabase domains
+      },
+    ],
+  },
 };
 
 export default nextConfig;
